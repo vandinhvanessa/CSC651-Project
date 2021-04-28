8f591d35c9540f4519b82c570bb9e2a3697f2469
Ubuntu <ubuntu@ip-172-31-24-251.us-east-2.compute.internal>
2021-04-28 23:14:22 +0000

    Updating to latest version

--- javascript/index.js
+++ javascript/index.js
@@ -5,11 +5,38 @@ $(document).ready(function () {
 
    fetchURL = "/employee/getEmployees/"
 
+   var employeeInfo;
+
    fetch(fetchURL, fetchOptions)
 	.then((data) => {
 	   return data.json();
 	})
 	.then((dataJson) => {
-	   console.log(dataJson)
+	   insertEmployeeInfo(dataJson);
 	})
 });
+
+function insertEmployeeInfo(employeeInfo){
+   var employeeHtml = "";
+
+   employeeInfo.forEach((employee) => {
+      employeeHtml += createEmployeeDiv(employee);
+   })
+
+   $("#employee-info").append(employeeHtml);
+}
+
+function createEmployeeDiv(employee){
+   var html = ""
+
+   html += "<div class='employee'>"
+   html += "<h4>Employee Name: " + employee.name + "</h4>"
+   html += "<h4>Position: " + employee.position + "</h4>"
+   html += "<h4>Salary: " + employee.salary + "</h4>"
+   html += "<h4>Email: " + employee.email + "</h4>"
+   html += "<h4>Phone: " + employee.phone + "</h4>"
+   html += "<hr/>"
+   html += "</div>"
+
+   return html
+}
